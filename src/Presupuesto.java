0ed3880d7a36bc5a6ffb53e4e76e07bbaef0edea
kgomez <kgomezcastiblanco@gmail.com>
2023-02-24 11:59:06 -0500

    se agrega la nueva clase Presupuesto

---
+++ src/Presupuesto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Presupuesto {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int spent, budget, op, contador;
+        budget = 100000;
+        op = 1;
+        contador = 0;
+
+        while (op==1 && contador == 0){
+            System.out.println("Ingrese el gasto");
+            spent = lectura.nextInt();
+            budget = budget - spent ;
+            System.out.println("el gasto fue "+ spent +" y su presupuesto restante es "+ budget);
+            System.out.println("Desea registrar otro gasto 1.si 2.no");
+            op = lectura.nextInt();
+            contador = contador + 1;
+        }
+        lectura.close();
+
+
+    }
+    
+}
